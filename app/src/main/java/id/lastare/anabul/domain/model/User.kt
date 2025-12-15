72b6c567a992a6905b2da877e4998ef15ab788f5
Lastare Program <hendrasaputra90@admin.smk.belajar.id>
2025-12-15 21:34:21 +0700

    Update User model with additional fields and default values
    - Add `created`, `updated`, `phone`, `isActive`, and `isAdmin` fields to the `User` data class.
    - Provide default values for all properties, including existing `id`, `email`, and `name` fields.

--- app/src/main/java/id/lastare/anabul/domain/model/User.kt
+++ app/src/main/java/id/lastare/anabul/domain/model/User.kt
@@ -1,7 +1,12 @@
 package id.lastare.anabul.domain.model
 
 data class User(
-    val id: String,
-    val email: String,
-    val name: String = ""
+    val id: String = "",
+    val created: Long = System.currentTimeMillis(),
+    val updated: Long = System.currentTimeMillis(),
+    val email: String = "",
+    val name: String = "",
+    val phone: String = "",
+    val isActive: Boolean = false,
+    val isAdmin: Boolean = false,
 )
